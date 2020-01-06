e5f5c948b9ceb1427e25c57f10bb520cc9dd1735
Runar Kristoffersen <runar@indicosys.com>
2020-01-06 08:16:25 +0100

    improve typesafety of metadata

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,7 +1,13 @@
 package metadata
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"github.com/btubbs/datetime"
+	"github.com/indicosystems/proxy/logger"
+	"github.com/sirupsen/logrus"
 	"strings"
+	"time"
 )
 
 const (
@@ -14,7 +20,10 @@ const (
 	// The ID of the container the file belongs to.
 	ParentId = "parentid"
 
-	// The RFC3339 compliant timestamp at which the file was created.
+	// The description of the parent
+	ParentDescription = "parentdescription"
+
+	// The ISO8601 compliant timestamp at which the file was created.
 	CreatedAt = "createdat"
 
 	// The mime type of the file.
@@ -26,24 +35,333 @@ const (
 	// Description given to the file
 	Description = "description"
 
-	// The checksum of the file.
+	// The Checksum of the file.
 	Checksum = "checksum"
+
+	// The type of Checksum used.
+	ChecksumType = "checksumtype" // – md5, sha256, crc etc.
 
 	// The name of the file on the file system.
 	Filename = "filename"
 
 	// The ID of the file at the external party
 	ExtId = "extid"
+
+	// The case number that an officer would use in their system
+	CaseNumber = "casenumber"
+
+	// The date of which the media was created. Might be different than the CreatedAt-field, which often represents the creation-date in a database
+	CapturedAt = "capturedat"
+
+	// Duration for the media, in case of audio/video. Always in ms.
+	Duration = "duration"
+
+	// The surname of the creator, often the officer.
+	CreatorSurname = "creatorsurname"
+
+	// The district of which the creator belongs.
+	CreatorDistrict = "creatordistrict"
+
+	// Textual representation of the location, like Madrid, or street address
+	LocationText = "locationtext"
+
+	// Tags for the item
+	Tags = "tags"
+
+	// Geo-Latitude.
+	Latitude = "latitude"
+
+	// Geo-Longitude
+	Longitude = "longitude"
+
+	Subjects = "subjects"
+
+	SubjectFirstName   = "subjectfirstname"
+	SubjectLastName    = "subjectlastname" // IR kaller dette SessionCustomerName ??
+	SubjectId          = "subjectid"       // Personnummer etc.
+	SubjectBirthdate   = "subjectbirthdate"
+	SubjectGender      = "subjectgender"
+	SubjectNationality = "subjectnationality"
+	SubjectWorkplace   = "subjectworkplace"
+	SubjectStatus      = "subjectstatus" // – Indikerer om subjektet er et vitne, offer, etc.
+	SubjectAddress     = "subjectaddress"
+	SubjectZip         = "subjectzip"
+	SubjectPostalCode  = "subjectpostalcode" // – IR: SessionCustomerIndex
+	SubjectCountry     = "subjectcountry"    // – Land for adresse
+	SubjectWorkPhone   = "subjectworkphone"
+	SubjectPhone       = "subjectphone"
+	SubjectMobile      = "subjectmobile"
+	SubjectPresent     = "subjectpresent" // – Er subjektet tilstede? boolean
+	AccountName        = "accountname"    // – IR: windows-kontoen
+	EquipmentID        = "equipmentid"    // – IR: InforPrefQuipmentID / DeviceInfo ?? også feltetd CustomDriverxbDeviceType
+	InterviewType      = "interviewtype"  // Itervju, avhør, etc.
+	Bookmarks          = "bookmarks"      // JSON
+	Attachments        = "attachments"    // id'er til ClientMediaID? hmm...
+	// A group-id can be specified by the client, if these items should be grouped.
+	// All uploads that share this key will be grouped. Not all backends supports this.
+	// This groupId is only used to link the files together, it is not stored on the backend.
+	GroupID = "groupid"
+	// If a group should be created/found on backend, this can be used to search for it.
+	// Note that GroupID is required anyway.
+	GroupName = "groupnam"
+
+	// Any additional notes
+	Notes = "notes"
+
+	// A unique identifier on the client. With attachments, this field is required.
+	ClientMediaId = "clientmediaid"
 )
+
+type Person struct {
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	Id          string `json:"id"`
+	Dob         string `json:"dob"`
+	Gender      string `json:"gender"`
+	Nationality string `json:"nationality"`
+	Workplace   string `json:"workplace"`
+	Status      string `json:"status"`
+	Address     string `json:"address"`
+	ZipCode     int    `json:"zip"`
+	Country     string `json:"country"`
+	WorkPhone   string `json:"workPhone"`
+	Phone       string `json:"phone"`
+	Mobile      string `json:"mobile"`
+	Present     bool   `json:"isPresent"`
+}
+
+type Parent struct {
+	Id          string
+	Name        string
+	Description string
+}
+
+type Creator struct {
+	District string
+	Person
+}
+
+type Type struct {
+	UserId        string
+	Parent        Parent
+	CreatedAt     *time.Time
+	CapturedAt    *time.Time
+	FileType      string
+	DisplayName   string
+	Description   string
+	Checksum      MetaChecksum
+	FileName      string
+	ExtId         string
+	CaseNumber    string
+	Duration      string
+	Creator       Creator
+	Location      Location
+	Subject       []Person
+	AccountName   string
+	EquipmentId   string
+	InterviewType string
+	Bookmarks     string
+	Attachments   string
+	Notes         string
+	ClientMediaId string
+	GroupId       string
+	GroupName     string
+}
+
+type MetaChecksum struct {
+	Value        string
+	ChecksumType string
+}
+
+type Location struct {
+	Text      string
+	Latitude  string
+	Longitude string
+}
+
+var l logrus.FieldLogger = logger.Get("metadata")
 
 type Metadata map[string]string
 type Mapper func(data Metadata) Metadata
 
+func (t Type) ConvertToMetaData() Metadata {
+	// TODO: Some of these nested types will result in error if empty
+	m := Metadata{}
+
+	m.Set(AccountName, t.AccountName)
+	m.Set(CaseNumber, t.CaseNumber)
+	if t.CreatedAt != nil {
+		m.Set(CreatedAt, t.CreatedAt.Format(time.RFC3339))
+	}
+	if t.CapturedAt != nil {
+		m.Set(CapturedAt, t.CapturedAt.Format(time.RFC3339))
+	}
+	if t.Subject != nil {
+
+		sJ, err := json.Marshal(t.Subject)
+		if err != nil {
+			l.Errorf("There was a problems Marhsalling the subjects-fields")
+		} else if len(sJ) > 0 {
+			subjects := base64.StdEncoding.EncodeToString(sJ)
+			if subjects != "" {
+				m.Set(Subjects, string(subjects))
+			}
+		}
+	}
+
+	m.Set(ClientMediaId, t.ClientMediaId)
+	m.Set(CreatorSurname, t.Creator.LastName)
+	m.Set(CreatorDistrict, t.Creator.District)
+	m.Set(GroupID, t.GroupId)
+	m.Set(GroupName, t.GroupName)
+	m.Set(ParentDescription, t.Parent.Description)
+	m.Set(ParentName, t.Parent.Name)
+	m.Set(Duration, t.Duration)
+	m.Set(FileType, t.FileType)
+	m.Set(DisplayName, t.DisplayName)
+	m.Set(Description, t.Description)
+	m.Set(Checksum, t.Checksum.Value)
+	m.Set(ChecksumType, t.Checksum.ChecksumType)
+	m.Set(Filename, t.FileName)
+	m.Set(ExtId, t.ExtId)
+	m.Set(LocationText, t.Location.Text)
+	m.Set(ParentId, t.Parent.Id)
+	m.Set(Longitude, t.Location.Longitude)
+	m.Set(Latitude, t.Location.Latitude)
+	m.Set(UserId, t.UserId)
+	m.Set(EquipmentID, t.EquipmentId)
+	m.Set(InterviewType, t.InterviewType)
+	m.Set(Notes, t.Notes)
+	// TODO:; Map atatchments, booksmarks.
+	for key, val := range m {
+		if val == "" {
+			delete(m, key)
+		}
+	}
+	return m
+
+}
+
+func (m Metadata) GetCreatedAtTime() *time.Time {
+	return parseDateSafely(m.getExact(CreatedAt))
+}
+func (m Metadata) GetCapturedAtTime() *time.Time {
+	return parseDateSafely(m.getExact(CapturedAt))
+}
+
+// Parses a ISO-8601-compliant string into a native time.
+func parseDateSafely(v string) *time.Time {
+	if v == "" {
+		return nil
+	}
+	t, err := datetime.Parse(v, time.UTC)
+	if err != nil {
+		return nil
+	}
+
+	return &t
+}
+
+func (m Metadata) ConvertToType() Type {
+	return Type{
+		ClientMediaId: m.getExact(ClientMediaId),
+		GroupId:       m.getExact(GroupID),
+		GroupName:     m.getExact(GroupName),
+		UserId:        m.getExact(UserId),
+		Parent: Parent{
+			Id:          m.getExact(ParentId),
+			Name:        m.getExact(ParentName),
+			Description: m.getExact(ParentDescription),
+		},
+		CreatedAt:   m.GetCreatedAtTime(),
+		CapturedAt:  m.GetCapturedAtTime(),
+		Duration:    m.getExact(Duration),
+		FileType:    m.getExact(FileType),
+		DisplayName: m.getExact(DisplayName),
+		Description: m.getExact(Description),
+		Checksum:    m.GetChecksum(),
+		FileName:    m.getExact(Filename),
+		ExtId:       m.getExact(ExtId),
+		CaseNumber:  m.getExact(CaseNumber),
+		Creator: Creator{
+			District: m.getExact(CreatorDistrict),
+			Person:   Person{LastName: m.getExact(CreatorSurname)},
+		},
+		Location: Location{
+			Text:      m.getExact(LocationText),
+			Latitude:  m.getExact(Latitude),
+			Longitude: m.getExact(Longitude),
+		},
+		Subject:       m.GetPerson(),
+		AccountName:   m.getExact(AccountName),
+		EquipmentId:   m.getExact(EquipmentID),
+		InterviewType: m.getExact(InterviewType),
+		Bookmarks:     m.getExact(Bookmarks),   // TODO: parse
+		Attachments:   m.getExact(Attachments), // TODO: parse
+		Notes:         m.getExact(Notes),
+	}
+}
+
+func (m Metadata) GetChecksum() MetaChecksum {
+	return MetaChecksum{
+		Value:        m.getExact(Checksum),
+		ChecksumType: m.getExact(ChecksumType),
+	}
+}
+
+func (m Metadata) GetPerson() []Person {
+	str := m.Get(Subjects)
+	if str == "" {
+		return nil
+	}
+	sDec, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return nil
+	}
+	var ps []Person
+	err = json.Unmarshal([]byte(sDec), &ps)
+	if err != nil {
+		return nil
+	}
+	return ps
+
+}
+func (m Metadata) getExact(key string) string {
+	if val, ok := m[key]; ok {
+		return strings.TrimSpace(val)
+	}
+	if val, ok := m[strings.ToLower(key)]; ok {
+		return strings.TrimSpace(val)
+	}
+	return ""
+}
+
+func (m Metadata) GetFileName() string {
+	return m.getExact(Filename)
+}
+
+// Can be used to get key/values with fallbacks. The keys to the left have precedence.
+func (m Metadata) Fallbacks(keys ...string) string {
+	for _, k := range keys {
+		if v := m.getExact(k); v != "" {
+			return v
+		}
+	}
+	return ""
+}
+
 func (m Metadata) Get(key string) string {
+	if val, ok := m[key]; ok {
+		return strings.TrimSpace(val)
+	}
 	lk := strings.ToLower(key)
+	if val, ok := m[lk]; ok {
+		return strings.TrimSpace(val)
+	}
 	for k, v := range m {
 		if strings.ToLower(k) == lk {
-			return v
+			l.Warnf("Return for-loop, this is aff deprecated behaviour, %s, %v", key, m[lk])
+			return strings.TrimSpace(v)
 		}
 	}
 	return ""
--- metadata/metadata_test.go
+++ metadata/metadata_test.go
@@ -1,8 +1,11 @@
 package metadata
 
 import (
+	"github.com/go-test/deep"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestMetadata_Get(t *testing.T) {
@@ -39,4 +42,201 @@ func TestMetadata_Map(t *testing.T) {
 	md.Map(key, key2)
 
 	assert.Equal(t, value, md[key2])
+}
+
+func TestMetadata_ConvertToType(t *testing.T) {
+	tests := []struct {
+		name string
+		m    Metadata
+		want Type
+	}{
+		// TODO: Add test cases.
+		{
+			"Should not fail on empty values",
+			Metadata{},
+			Type{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.ConvertToType(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ConvertToType() = \n%+v, \nwant \n%+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestType_ConvertToMetaData(t1 *testing.T) {
+	type fields struct {
+		UserId        string
+		Parent        Parent
+		CreatedAt     *time.Time
+		CapturedAt    *time.Time
+		FileType      string
+		DisplayName   string
+		Description   string
+		Checksum      MetaChecksum
+		FileName      string
+		ExtId         string
+		CaseNumber    string
+		Duration      string
+		Creator       Creator
+		Location      Location
+		Subject       []Person
+		AccountName   string
+		EquipmentId   string
+		InterviewType string
+		Bookmarks     string
+		Attachments   string
+		Notes         string
+		ClientMediaId string
+		GroupId       string
+	}
+	createdat := time.Date(2019, 12, 24, 0, 0, 0, 0, time.UTC)
+	tests := []struct {
+		name   string
+		fields Type
+		want   Metadata
+	}{
+		{
+			"Should not fail on empty values",
+			Type{},
+			Metadata{},
+		},
+		{
+			"Should not fail on basic values",
+			Type{
+				GroupId:   "1234",
+				CreatedAt: &createdat,
+			},
+			Metadata{
+				GroupID:   "1234",
+				CreatedAt: "2019-12-24T00:00:00Z",
+			},
+		},
+		{
+			"should parse all fields correctly",
+			Type{
+				UserId: "1111",
+				Parent: Parent{
+					Id:          "1234",
+					Name:        "Sigma",
+					Description: "With his mavericks",
+				},
+				CreatedAt:   &createdat,
+				CapturedAt:  &createdat,
+				FileType:    "image/jpeg",
+				DisplayName: "Dogs and floating heads",
+				Description: "Illegal weapons",
+				Checksum: MetaChecksum{
+					Value:        "1234-ABC",
+					ChecksumType: "Imaginary",
+				},
+				FileName:   "01.jpeg",
+				ExtId:      "9999",
+				CaseNumber: "8888",
+				Duration:   "30",
+				Creator: Creator{
+					District: "Lab",
+					Person: Person{
+						LastName: "Light",
+						//FirstName:   "Thomas",
+						//Id:          "1234",
+						//Dob:         "20xx",
+						//Gender:      "Male",
+						//Nationality: "Canadian",
+						//Workplace:   "Robot Institute of Technology",
+						//Status:      "",
+						//Address:     "",
+						//ZipCode:     0,
+						//Country:     "",
+						//WorkPhone:   "",
+						//Phone:       "",
+						//Mobile:      "",
+						//Present:     true,
+					},
+				},
+				Location: Location{
+					Text:      "Some place",
+					Latitude:  "8",
+					Longitude: "7",
+				},
+				Subject: []Person{
+					{
+						FirstName:   "Unknown",
+						LastName:    "Sigma",
+						Id:          "666",
+						Dob:         "unknown",
+						Gender:      "Male",
+						Nationality: "American",
+						Workplace:   "Maverick Hunters",
+						Status:      "Suspect",
+						Address:     "Unknown",
+						ZipCode:     0,
+						Country:     "USA",
+						WorkPhone:   "6786762",
+						Phone:       "1267363",
+						Mobile:      "162363",
+						Present:     true,
+					},
+				},
+				AccountName:   "Supa-Computa",
+				EquipmentId:   "XC66",
+				InterviewType: "???",
+				//Bookmarks:     "", // TODO: add test for booksmarks, attachments, etc.
+				//Attachments:   "",
+				Notes:         "Some random notes",
+				ClientMediaId: "1234",
+				GroupId:       "4321",
+			},
+			Metadata{
+				Subjects:      "W3siZmlyc3ROYW1lIjoiVW5rbm93biIsImxhc3ROYW1lIjoiU2lnbWEiLCJpZCI6IjY2NiIsImRvYiI6InVua25vd24iLCJnZW5kZXIiOiJNYWxlIiwibmF0aW9uYWxpdHkiOiJBbWVyaWNhbiIsIndvcmtwbGFjZSI6Ik1hdmVyaWNrIEh1bnRlcnMiLCJzdGF0dXMiOiJTdXNwZWN0IiwiYWRkcmVzcyI6IlVua25vd24iLCJ6aXAiOjAsImNvdW50cnkiOiJVU0EiLCJ3b3JrUGhvbmUiOiI2Nzg2NzYyIiwicGhvbmUiOiIxMjY3MzYzIiwibW9iaWxlIjoiMTYyMzYzIiwiaXNQcmVzZW50Ijp0cnVlfV0=",
+				AccountName:   "Supa-Computa",
+				InterviewType: "???",
+				//Bookmarks:         "", // TODO: add test for booksmarks, attachments, etc.
+				//Attachments:       "",
+				Notes:             "Some random notes",
+				ClientMediaId:     "1234",
+				ExtId:             "9999",
+				CaseNumber:        "8888",
+				Duration:          "30",
+				CreatedAt:         "2019-12-24T00:00:00Z",
+				CapturedAt:        "2019-12-24T00:00:00Z",
+				FileType:          "image/jpeg",
+				DisplayName:       "Dogs and floating heads",
+				Description:       "Illegal weapons",
+				UserId:            "1111",
+				CreatorSurname:    "Light",
+				CreatorDistrict:   "Lab",
+				EquipmentID:       "XC66",
+				ParentDescription: "With his mavericks",
+				ParentId:          "1234",
+				ParentName:        "Sigma",
+				ChecksumType:      "Imaginary",
+				Checksum:          "1234-ABC",
+				Latitude:          "8",
+				Longitude:         "7",
+				LocationText:      "Some place",
+				GroupID:           "4321",
+				Filename:          "01.jpeg",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			t := tt.fields
+			got := t.ConvertToMetaData()
+			if diff := deep.Equal(got, tt.want); diff != nil {
+				t1.Errorf("Did not match wanted result: \n diff %+v, \nGot: %+v, \nWanted: %+v", diff, got, tt.want)
+			}
+			m := got.ConvertToType()
+			if diff := deep.Equal(m, t); diff != nil {
+				t1.Errorf("Convert to metadata and back again did not produce the same result\n diff: %+v \nGot: %+v \n Expected result: %+v", diff, m, t)
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t1.Errorf("ConvertToMetaData() => ConvertToType = \n%v, want \n%v", got, tt.want)
+			//}
+
+		})
+	}
 }
